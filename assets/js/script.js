14be478f1368af40f658371b07ea47f9d1216c2a
Andres <zleckzxd@gmail.com>
2021-05-12 20:20:00 -0400

    pre_release

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+$(function(){
+    $("a").click(function(event){
+        if (this.hash !=="") {
+            event.preventDefault();
+            var gato = this.hash;
+            $("html, body").animate({
+                scrollTop: $(gato).offset().top
+            }, 800, function(){
+                window.location.hash = gato;
+            })
+        }
+    })
+});
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
